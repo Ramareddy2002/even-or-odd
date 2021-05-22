cac72ec2c4a526e2099320492f8e72b510c04ad2
Ramareddy2002 <84448206+Ramareddy2002@users.noreply.github.com>
2021-05-22 13:46:42 +0530

    Add files via upload

---
+++ p0111.c
@@ -0,0 +1,25 @@
+'''python program to given number is odd or not ven
+
+a=int(input('enter value')) 
+print(a) 
+if (a℅2=0):
+       print(number is even) 
+       print(u entered even number) 
+else :  
+       print(number is odd)
+       print(u entered odd number) 
+    
+    
+ '''
+expected output
+enter value 4
+4
+number is even
+u entered even number
+'''
+''' 
+expected output
+enter value 9
+9
+number is odd
+u entered odd numt
